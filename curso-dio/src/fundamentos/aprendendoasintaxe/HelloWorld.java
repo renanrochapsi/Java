9590a54cd2f9e6d98cba52cb662552eaf38a9351
renanrochapsi <renanrocha.psi@gmail.com>
2023-10-04 14:36:00 -0300

    Initial commit

--- curso-dio/src/fundamentos/aprendendoasintaxe/HelloWorld.java
+++ curso-dio/src/fundamentos/aprendendoasintaxe/HelloWorld.java
@@ -1,6 +1,6 @@
 package fundamentos.aprendendoasintaxe;
 public class HelloWorld {
-    public static  void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Hello, world");
     }
 
---
+++ curso-dio/src/fundamentos/aprendendoasintaxe/contaterminal/ContaBanco.java
@@ -0,0 +1,12 @@
+package fundamentos.aprendendoasintaxe.contaterminal;
+public class ContaBanco {
+    public static void main(String[] args){
+        System.out.println("Por favor, digite sua agência");
+        String agencia = args [0];
+        String conta = args[1];
+
+    }
+
+}
+
+// SAIDA: Hello, world
