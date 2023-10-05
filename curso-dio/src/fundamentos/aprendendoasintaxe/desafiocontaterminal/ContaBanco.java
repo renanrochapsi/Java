3b8c7816ce9cf72d33ac47d95f5e58c1ef51d027
renanrochapsi <renanrocha.psi@gmail.com>
2023-10-04 22:55:14 -0300

    Initial commit

--- curso-dio/src/fundamentos/aprendendoasintaxe/desafiocontaterminal/ContaBanco.java
+++ curso-dio/src/fundamentos/aprendendoasintaxe/desafiocontaterminal/ContaBanco.java
@@ -5,19 +5,31 @@ public class ContaBanco {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Digite o nº da sua sua agência: ");
+        String agencia  = scanner.nextLine();
+
         System.out.print("Digite o nº da sua conta: ");
         int conta  = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.print("Digite o nº da sua sua agência: ");
-        String agencia  = scanner.nextLine();
+        System.out.println("Sua agência e conta são: " + agencia + " e " + conta);
 
-        System.out.println("Sua conta e agência são: " + conta + " e " + agencia);
 
-        String senha = scanner.nextLine();
+        System.out.print("Digite o nº da sua sua senha: ");
+        int senha  = scanner.nextInt();
+        scanner.nextLine();
+
+
+        if (senha == 1547) {
+            System.out.println("Parabéns, você está conectado");
+        } else {
+            System.out.println("Senha incorreta");
+        }
+
 
         scanner.close();
     }
 
 }
 
+// Minha contribuição ao código irá ser colocar uma senha no sistema
